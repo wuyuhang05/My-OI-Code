d57db515361e953042bf070d044768e9af06cba6
wuyuhang422 <wuyuhang422@gmail.com>
2020-07-09 21:07:34 +0800

    upd

--- LOJ/6405.cpp
+++ LOJ/6405.cpp
@@ -1,3 +1,11 @@
+
+// Problem : #6405. 「ICPC World Finals 2018」征服世界
+// Contest : LibreOJ
+// URL : https://loj.ac/problem/6405
+// Memory Limit : 1024 MB
+// Time Limit : 8000 ms
+// Powered by CP Editor (https://github.com/cpeditor/cpeditor)
+
 #include<bits/stdc++.h>
 #include<ext/pb_ds/priority_queue.hpp>
 
@@ -13,8 +21,8 @@
 #define FOR(i,a,b) for(int i = a;i <= b;++i)
 #define ROF(i,a,b) for(int i = a;i >= b;--i)
 #define DEBUG(x) std::cerr << #x << '=' << x << std::endl
-#define int LL
-const int MAXN = 3e5 + 5;
+
+const int MAXN = 2e5 + 5;
 
 struct Edge{
 	int to,w,nxt;
@@ -31,7 +39,7 @@ int dep[MAXN];
 inline void dfs1(int v,int fa=0){
 	for(int i = head[v];i;i = e[i].nxt){
 		if(e[i].to == fa) continue;
-		dep[e[i].to] = dep[v]+e[i].w;dfs1(e[i].to,v);
+		dep[e[i].to] = dep[v]+1;dfs(e[i].to,v);
 	}
 }
 
@@ -42,34 +50,21 @@ int ans;
 __gnu_pbds::priority_queue<int,std::greater<int> > q1[MAXN],q2[MAXN];
 
 inline void dfs2(int v,int fa=0){
-    if(a[v] > 0) while(a[v]--) q1[v].push(dep[v]);
-    else if(a[v] < 0) while(a[v]++) q2[v].push(dep[v]-1e12);
-    for(int i = head[v];i;i = e[i].nxt){
-        if(e[i].to == fa) continue;
-        dfs2(e[i].to,v);
-        q1[v].join(q1[e[i].to]);
-        q2[v].join(q2[e[i].to]);
-    }
-    while(!q1[v].empty() && !q2[v].empty() && q1[v].top()+q2[v].top()-2*dep[v] < 0){
-        int h = q1[v].top(),m = q2[v].top();q1[v].pop();q2[v].pop();
-        ans += h+m-2*dep[v];
-        q1[v].push(2*dep[v]-m);q2[v].push(2*dep[v]-h);
-    }
+	flak;wjfl;akqfkawl;kfl;awkfl;w
 }
 
-signed main(){
-	scanf("%lld",&n);
+int main(){
+	scanf("%d",&n);
 	FOR(i,2,n){
-		int u,v,w;scanf("%lld%lld%lld",&u,&v,&w);
+		int u,v,w;scanf("%d%d%d",&u,&v,&w);
 		add(u,v,w);
 	}
-    int cnt = 0;
 	FOR(i,1,n){
-        int x,y;scanf("%lld%lld",&x,&y);
-        a[i] = x-y;if(a[i] < 0) cnt -= a[i];
+		int x,y;scanf("%d%d",&x,&y);
+		a[i] = x-y;
 	}
 	dfs1(1);
 	dfs2(1);
-	printf("%lld\n",ans+1000000000000*cnt);
+	printf("%d\n",ans);
 	return 0;
 }
--- ZROI/880.cpp
+++ ZROI/880.cpp
@@ -31,7 +31,96 @@
 #define ROF(i,a,b) for(Re int i = a;i >= b;--i)
 #define DEBUG(x) std::cerr << #x << '=' << x << std::endl
 
-int main(){
+const int MAXN = 1e6 + 5;
+const int MAXM = 168;
+std::bitset<MAXM+1> b[MAXN];
+int ts[MAXM+1];
+std::bitset<MAXM+1> x;
 
+inline void insert(int big,int ps=0){
+    if(big != 1){
+        if(!ts[big]){
+            ts[big] = ps;
+            b[big] = x;
+            return;
+        }
+        else{
+            std::swap(b[big],x);
+            std::swap(ts[big],ps);
+            x ^= b[big];
+        }
+    }
+    ROF(i,MAXM,1){
+        if(x[i]){
+            if(!ts[i]){
+                ts[i] = ps;
+                b[i] = x;
+                return;
+            }
+            else if(ps <= ts[i]){
+                std::swap(b[i],x);
+                std::swap(ts[i],ps);
+                x ^= b[i];
+            }
+            else x ^= b[i];
+        }
+    }
+}
+
+inline int query(int big){
+    int res = 0;
+    if(big != 1){
+        if(ts[big]){
+            x ^= b[big];
+            res = std::max(res,ts[big]);
+        }
+        else return -1;
+    }
+    ROF(i,MAXM,1){
+        if(x[i]){
+            if(!ts[i]) return -1;
+            res = std::max(res,ts[i]);
+            x ^= b[i];
+        }
+    }
+    return res;
+}
+
+bool p[MAXN];
+int prime[MAXN],cnt;
+int f[MAXN],d[MAXN];
+
+inline void prework(){
+    FOR(i,2,MAXN-1){
+        if(!p[i]) prime[++cnt] = i,f[i] = cnt,d[i] = i;
+        for(int j = 1;j <= cnt && i*prime[j] < MAXN;++j){
+            p[i*prime[j]] = 1;d[i*prime[j]] = prime[j];
+            if(!(i%prime[j])) break;
+        }
+    }
+}
+
+int n,ans[MAXN];
+
+int main(){
+    prework();
+    scanf("%d",&n);
+    ROF(i,n,1){
+        std::bitset<MAXM+1> bk;int t = i;x.reset();
+        while(t != 1){
+            if(f[d[t]] > MAXM) break;
+            x[f[d[t]]].flip();
+            t /= d[t];
+        }
+        bk = x;
+        ans[i] = query(t);
+        if(ans[i] != -1) ans[i] = std::max(ans[i],i);
+        x = bk;
+        insert(t,i);
+    }
+    std::vector<int> S;
+    FOR(i,1,n) if(ans[i] == n) S.pb(i);
+    printf("%d\n",(int)S.size());
+    for(auto x:S) printf("%d\n",x);
     return 0;
 }
